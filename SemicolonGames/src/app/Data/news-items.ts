0b3493a05d89d9e181dd50201e64f842002f1cb9
Unknown <jeroen.thomas@ymail.com>
2020-01-12 15:06:40 +0100

    English text for these added.

--- SemicolonGames/src/app/Data/news-items.ts
+++ SemicolonGames/src/app/Data/news-items.ts
@@ -5,7 +5,13 @@ export const NewsItemsList = [{
 },
 {
     title: 'Website published',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    content: 'This is the website for Red Jellyfish games. I hope this is a website you"ll visit more often. Please explore the website and give feedbback if you find something odd. ',
+    date: new Date(2020, 1, 1)
+},
+{
+    title: 'Update week 2',
+    content: `During the first two weeks of 2020 we have updated the website and worked on an inital version of a logo. Furthermore, hard deadlines have been set by teh development team.
+    The controls for the mobiele devices also work and have been tested thoroughly. We are currently working on some character models and making the menu's and some more in-game elements.`,
     date: new Date(2020, 1, 1)
 }
 ];
